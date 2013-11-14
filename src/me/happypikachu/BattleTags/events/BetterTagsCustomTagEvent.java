8ed98189be7edc4948eae1ba4f03441e2e058795
jose.rob.jr <jose.rob.jr@gamemods.com.br>
2013-11-14 03:04:38 -0300

    Added an event to allow other plugins to change the resulting tag

---
+++ src/me/happypikachu/BattleTags/events/BetterTagsCustomTagEvent.java
@@ -0,0 +1,37 @@
+package me.happypikachu.BattleTags.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class BetterTagsCustomTagEvent extends Event {
+	private static final HandlerList handlers = new HandlerList();
+	@Override public HandlerList getHandlers() { return handlers; }
+	public static HandlerList getHandlerList() { return handlers; }
+	
+	private String player, seenPlayer, tag;
+	
+	public BetterTagsCustomTagEvent(String player, String seenPlayer, String tag) {
+		this.player = player;
+		this.seenPlayer = seenPlayer;
+		this.tag = tag;
+	}
+	
+	public String getPlayer() {
+		return player;
+	}
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+	public String getSeenPlayer() {
+		return seenPlayer;
+	}
+	public void setSeenPlayer(String seenPlayer) {
+		this.seenPlayer = seenPlayer;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+}
