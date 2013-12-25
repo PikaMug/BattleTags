213805de242ad87dbc93ffdf72e088f1fa4569fe
brord van wierst <kwek20@gmail.com>
2013-12-25 12:16:13 +0100

    Small update on some files for the tabmanager

--- src/me/happypikachu/BattleTags/managers/BattleTagsTabManager.java
+++ src/me/happypikachu/BattleTags/managers/BattleTagsTabManager.java
@@ -1,5 +1,9 @@
 package me.happypikachu.BattleTags.managers;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import me.happypikachu.BattleTags.BattleTags;
 
 import org.bukkit.Bukkit;
@@ -49,18 +53,19 @@ public class BattleTagsTabManager extends TagsManager implements Listener {
 	public void update(Player player) {
 		TabAPI.clearTab(player);
 		Player[] players = Bukkit.getServer().getOnlinePlayers();
+		
+		List<Player> orderedplayers = new LinkedList<Player>();
 		if (grouping){
 			
 		} else {
-			for (int i=0; i < players.length; i++){
-				if (i > (TabAPI.getHorizSize()*TabAPI.getVertSize()) ) break;
-				
-				//i = 20, 20/3 == 
-				System.out.println(Math.floor(i/3) + " | " + i%3 + " | " + getTag(players[i], player));
-				TabAPI.setTabString(plugin, players[i], (int)Math.floor(i/3), i%3, getTag(players[i], player));
-			}
+			orderedplayers = Arrays.asList(players);
 		}
-		TabAPI.updateAll();
+		
+		for (int i=0; i < orderedplayers.size(); i++){
+			if (i > (TabAPI.getHorizSize()*TabAPI.getVertSize()) ) break;
+			TabAPI.setTabString(plugin, player, ((int)Math.floor(i/3)), i%3, getTag(player, orderedplayers.get(i)));
+		}
+		TabAPI.updatePlayer(player);
 	}
 	
 }
